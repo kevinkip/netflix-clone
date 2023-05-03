d958cbd739c134deb92bf521a73e5150ff470385
Kevin Kipkemboi <theptjhub@gmail.com>
2023-05-02 19:50:03 -0700

    Sign in and email form

---
+++ pages/auth.tsx
@@ -0,0 +1,35 @@
+import Input from "@/components/Input";
+
+const Auth = () => {
+    return (
+        //styling with tailwindcss
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full bg-opacity-50">
+                {/* padding in x and y axis */}
+                <nav className="px-12 py-5">
+                    <picture>
+                        <img src="/images/logo.png" alt="Logo" className="h-12" />
+                    </picture>
+                </nav>
+                {/* container with the inputs, buttons and other types of logins */}
+                <div className="flex justify-center">
+                    {/* black background w/ opacity of 70. 16px padding all around. centered along the center of container's axis.
+                    margin-top of 2. On large screens, have a width of 2/5ths and a max-width of medium, rounded corners-medium 
+                    and full width of element. */}
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                    {/* margin-bottom (mb) with semi-bold font */}
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            Sign in
+                        </h2>
+                        {/* hold our inputs */}
+                        <div className="flex flex-col gap-4">
+                            <Input/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
