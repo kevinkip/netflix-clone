1556e4a93f345dcf45a6fb871a8e34913a17b2ca
Kevin Kipkemboi <theptjhub@gmail.com>
2023-05-04 11:09:56 -0700

    added movie cards and stylized

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
 
 //checking if available session exists. If not, redirect us to /auth.
 export async function getServerSideProps(context: NextPageContext){
@@ -23,16 +25,16 @@ export async function getServerSideProps(context: NextPageContext){
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
   //fetch user
   // const { data: user } = useCurrentUser();
   return (
     <>
     <Navbar />
-
-      {/* <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-      <p className='text-white'>Logged in as: {user?.email}</p>
-      <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout!</button> */}
-    <Billboard />      
+    <Billboard />
+    <div className='pg-40'>
+      <MovieList title="Trending Now" data={movies} />     
+      </div> 
     </>
 
   )
