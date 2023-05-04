c7dc5f99ad4fe08ee948323660cee292d059355a
Kevin Kipkemboi <theptjhub@gmail.com>
2023-05-04 11:34:02 -0700

    added favorites button with styling

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BsFillPlayFill } from 'react-icons/bs'
+import { BsFillPlayFill } from 'react-icons/bs';
+import FavoriteButton from "./favoriteButton";
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -85,6 +86,7 @@ const MovieCard: React.FC<MovieCardProps> =({
                         onClick={() => {}}>
                             <BsFillPlayFill size={30} />
                         </div>
+                        <FavoriteButton movieId={data?.id}/>
                     </div>
                     <p className="text-green-400 font-semibold mt-4">
                         New <span className="text-white">2023</span>
